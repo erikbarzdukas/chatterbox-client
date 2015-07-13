06fae2686651ea8fad06cb02bfc28b21f97d51c8
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-07-13 16:06:30 -0700

    Displaying user and text

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,13 +1,34 @@
 // YOUR CODE HERE:
+
+/**
+ * Escape the data, add it to page if user is in
+ * current chat room. Also bold content if the 
+ * speaker is a user's friend
+ */
+var updateMessages = function(data){
+    console.log(data);
+    _.each(data.results, function(value, index, results){
+      var safeText = document.createTextNode(value.text);
+      var safeUser = document.createTextNode(value.username);
+      var safeRoom = document.createTextNode(value.roomname);
+      var $messageDiv = $('<div></div>');
+      var $user = $('<b></b>');
+      var $message = $('<p></p>');
+
+      $user.append(safeUser);
+      $message.append(safeText);
+      $messageDiv.append($user);
+      $messageDiv.append($message);
+      console.log($messageDiv);
+      $('#messages').append($messageDiv);
+    }); 
+  }
+
 $.ajax({
   url: 'https://api.parse.com/1/classes/chatterbox',
   type: 'GET',
   dataType: 'json',
-  success: function(data){
-    console.log(data);
-    _.each(data.results, function(value, index, results){
-     var safeText = document.createTextNode(value.text);
-     $('#messages').append(safeText);
-    }); 
-  }
-})
+  success: updateMessages
+});
+
+
