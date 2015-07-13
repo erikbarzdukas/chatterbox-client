2e8e3c21b1f38104ae98e4985f247f722a50d8df
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-07-13 15:30:42 -0700

    GET message objects and display texts in safe way

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,13 @@
 // YOUR CODE HERE:
-
+$.ajax({
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'GET',
+  dataType: 'json',
+  success: function(data){
+    console.log(data);
+    _.each(data.results, function(value, index, results){
+     var safeText = document.createTextNode(value.text);
+     $('#messages').append(safeText);
+    }); 
+  }
+})
