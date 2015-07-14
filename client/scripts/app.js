76122a0fb6ef4703c8c5cae87934abf1281df588
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-07-13 19:41:22 -0700

    All tests except the last pass

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,34 +1,84 @@
 // YOUR CODE HERE:
 
-/**
- * Escape the data, add it to page if user is in
- * current chat room. Also bold content if the 
- * speaker is a user's friend
- */
-var updateMessages = function(data){
-    console.log(data);
-    _.each(data.results, function(value, index, results){
-      var safeText = document.createTextNode(value.text);
-      var safeUser = document.createTextNode(value.username);
-      var safeRoom = document.createTextNode(value.roomname);
-      var $messageDiv = $('<div></div>');
-      var $user = $('<b></b>');
-      var $message = $('<p></p>');
+var App = function(){
+  this.friends = [];
+}
+App.prototype.constructor = App;
 
-      $user.append(safeUser);
-      $message.append(safeText);
-      $messageDiv.append($user);
-      $messageDiv.append($message);
-      console.log($messageDiv);
-      $('#messages').append($messageDiv);
-    }); 
-  }
+App.prototype.init = function(){
+  var that = this;
+  $('.username').on("click", function(e){
+    that.addFriend($(e.target).text());
+  });
 
-$.ajax({
-  url: 'https://api.parse.com/1/classes/chatterbox',
-  type: 'GET',
-  dataType: 'json',
-  success: updateMessages
-});
+  $('form').on("submit", function(e){
+    console.log('Event listener heard submit');
+    e.preventDefault();
+    that.handleSubmit($('#message').val());
+    $('#message').val('');
+  });
+}
+
+App.prototype.send = function(message){
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function(data){
+      console.log('chatterbox: Message sent');
+    },
+    error: function(data){
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+}
+
+App.prototype.fetch = function() {
+  $.ajax({
+    success: function(data){
+     console.log('chatterbox: Sent message');
+    } 
+  });
+}
+
+App.prototype.clearMessages = function(){
+  $('#chats').remove();
+}
+
+App.prototype.addMessage = function(message){
+    var safeText = document.createTextNode(message.text);
+    var safeUser = document.createTextNode(message.username);
+    var safeRoom = document.createTextNode(message.roomname);
+    var $messageDiv = $('<div></div>');
+    var $user = $('<b class="username"></b>');
+    var $message = $('<p></p>');
+    var $chats = $('#chats');
+    if($chats.length < 1){
+      $chats = $('<div id="chats"></div>');
+    }
+
+    $user.append(safeUser);
+    $message.append(safeText);
+    $messageDiv.append($user);
+    $messageDiv.append($message);
+    $chats.append($messageDiv);
+    $('#main').append($chats);
+
+}
+
+App.prototype.addRoom = function(room){
+  var $room = $('<option>' + room + '</option>');
+  $('#roomSelect').append($room);
+}
+
+App.prototype.addFriend = function(){
+}
+
+App.prototype.handleSubmit = function(message){
+  this.send(message);
+}
+
+var app = new App();
 
 
